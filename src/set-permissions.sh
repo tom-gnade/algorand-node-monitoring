54aa9f3f4de96a9ef3003c92bde432f839552295
Tom Gnade <tom.gnade@gmail.com>
2024-11-11 13:39:43 -0500

    Remove junk and add .gitignore to ./data

--- src/set-permissions.sh
+++
@@ -1,8 +0,0 @@
-# -~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-+-~-
-# !/bin/bash
-
-# note: chmod +x set-permissions.sh && ./set-permissions.sh;
-
-# apply permissions to data directories
-chown -R nobody:nogroup ./prometheus/data # Prometheus
-chown -R 1000:1000 ./elasticsearch/data #Elasticsearch
--- src/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gosu 65534:65534 goal kmd start
-exec gosu 65534:65534 algod -d /algod/data
