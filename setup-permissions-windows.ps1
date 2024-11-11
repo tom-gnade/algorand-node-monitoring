968a5dfc1f40fc04e4d83f197de4669d250b6af6
Tom Gnade <tom.gnade@gmail.com>
2024-11-11 15:46:57 -0500

    Fixed the powershell version

--- setup-permissions-windows.ps1
+++ setup-permissions-windows.ps1
@@ -1,44 +1,44 @@
 # Check if running as Administrator
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Warning "Please run this script as Administrator!"
-    Break
+   Write-Warning "Please run this script as Administrator!"
+   Break
 }
 
 Write-Host "Setting up permissions for AlgoMon persistent storage on Windows..."
 
 # Create data directories if they don't exist
 $directories = @(
-    "elasticsearch\data",
-    "prometheus\data",
-    "grafana\data"
+   "elasticsearch\data",
+   "prometheus\data",
+   "grafana\data"
 )
 
 foreach ($dir in $directories) {
-    if (-not (Test-Path $dir)) {
-        New-Item -ItemType Directory -Force -Path $dir | Out-Null
-        Write-Host "Created directory: $dir"
-    }
+   if (-not (Test-Path $dir)) {
+      New-Item -ItemType Directory -Force -Path $dir | Out-Null
+      Write-Host "Created directory: $dir"
+   }
 }
 
 # Set permissions for each directory
 foreach ($dir in $directories) {
-    Write-Host "Setting permissions for $dir..."
-    try {
-        $acl = Get-Acl $dir
-        $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
-            "Users",
-            "FullControl",
-            "ContainerInherit,ObjectInherit",
-            "None",
-            "Allow"
-        )
-        $acl.SetAccessRule($accessRule)
-        Set-Acl $dir $acl
-        Write-Host "Successfully set permissions for $dir"
-    }
-    catch {
-        Write-Error "Failed to set permissions for $dir: $_"
-    }
+   Write-Host "Setting permissions for $dir..."
+   try {
+      $acl = Get-Acl $dir
+      $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
+         "Users",
+         "FullControl",
+         "ContainerInherit,ObjectInherit",
+         "None",
+         "Allow"
+      )
+      $acl.SetAccessRule($accessRule)
+      Set-Acl $dir $acl
+      Write-Host "Successfully set permissions for $dir"
+   }
+   catch {
+      Write-Error ("Failed to set permissions for {0}: {1}" -f $dir, $PSItem.ToString())
+   }
 }
 
 Write-Host "`nDone! All permissions have been set correctly."
